56e8cc71046368f69fc45f11cf497d7b5b525c2a
kcnsmoothie <kcnsmoothie05@gmail.com>
2025-10-01 14:21:29 +0900

    Test : Service 테스트할 수 있는 코드를 작성

--- src/test/java/hello/hello_spring/service/MemberServiceTest.java
+++ src/test/java/hello/hello_spring/service/MemberServiceTest.java
@@ -1,23 +1,62 @@
 package hello.hello_spring.service;
 
 import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
     //테스트 코드같은 경우는 한국어로 써도 됨. 빌드 시에 포함 안되니까요
+    MemberService memberService = new MemberService();
 
     @Test
     void join() {
         //given
         Member member = new Member();
-        member.setName();
+        member.setName("spring");
 
         //when
+        Long saveId = memberService.join(member);
 
         //then
+        Member findMember = memberService.findOne(saveId).get();
+        Assertions.assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다");
+
+/*        try {
+            memberService.join(member2);
+            fail();
+        } catch (IllegalAccessError e) {
+            Assertions.assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        }
+*/
+
+        //then
+
+    }
+
+    private static Member getMember() {
+        Member member1 = new Member();
+        return member1;
     }
 
     @Test
